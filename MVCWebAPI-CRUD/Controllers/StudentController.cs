1437c8097cc9010cb467496940e8fe97f08653bf
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2021-03-20 21:19:47 +0530

    Basics

--- MVCWebAPI-CRUD/Controllers/StudentController.cs
+++ MVCWebAPI-CRUD/Controllers/StudentController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using MVCWebAPI_CRUD.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -11,9 +15,31 @@ namespace MVCWebAPI_CRUD.Controllers
     public class StudentController : ApiController
     {
         [HttpGet]
-        public string StudentDetails()
+        public Student StudentDetails(int id)
         {
-            return "Test data";
+
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("StudentDetails", connection);
+            command.Parameters.AddWithValue("ID", id);
+            command.CommandType = CommandType.StoredProcedure;
+            SqlDataReader sqlDataReader = command.ExecuteReader();
+            sqlDataReader.Read();
+
+            Student student = new Student();
+
+            student.ID = Convert.ToInt32(sqlDataReader["ID"]);
+            student.Name = sqlDataReader["Name"].ToString();
+            student.Malayalam = sqlDataReader["Malayalam"].ToString();
+            student.English = sqlDataReader["English"].ToString();
+            student.Hindi = sqlDataReader["Hindi"].ToString();
+            student.Email = sqlDataReader["Email"].ToString();
+            student.Total = Convert.ToInt32(sqlDataReader["Total"]);
+            student.Average =Convert.ToDecimal( sqlDataReader["Average"]);
+            student.Password = sqlDataReader["Password"].ToString();
+            student.CreatedDate =Convert.ToDateTime( sqlDataReader["CreatedDate"]);
+
+            return student;
         }
         [HttpGet]
         public string StudentSave()
---
+++ MVCWebAPI-CRUD/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWebAPI_CRUD.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Malayalam { get; set; }
+        public string English { get; set; }
+        public string Hindi { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int? Total { get; set; }
+        public decimal? Average { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
