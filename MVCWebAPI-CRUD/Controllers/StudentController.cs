784dc9a200635a122c861b6de05ba22b00e7d272
Akhil Raj <akhil.raj@appsteamtechnologies.com>
2021-03-20 21:39:42 +0530

    Code updated

--- MVCWebAPI-CRUD/Controllers/StudentController.cs
+++ MVCWebAPI-CRUD/Controllers/StudentController.cs
@@ -5,16 +5,16 @@ using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
-using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
 
 namespace MVCWebAPI_CRUD.Controllers
 {
     public class StudentController : ApiController
     {
-        [HttpGet]
+        [System.Web.Http.HttpGet]
         public Student StudentDetails(int id)
         {
 
@@ -35,16 +35,45 @@ namespace MVCWebAPI_CRUD.Controllers
             student.Hindi = sqlDataReader["Hindi"].ToString();
             student.Email = sqlDataReader["Email"].ToString();
             student.Total = Convert.ToInt32(sqlDataReader["Total"]);
-            student.Average =Convert.ToDecimal( sqlDataReader["Average"]);
+            student.Average = Convert.ToDecimal(sqlDataReader["Average"]);
             student.Password = sqlDataReader["Password"].ToString();
-            student.CreatedDate =Convert.ToDateTime( sqlDataReader["CreatedDate"]);
+            student.CreatedDate = Convert.ToDateTime(sqlDataReader["CreatedDate"]);
 
             return student;
         }
-        [HttpGet]
-        public string StudentSave()
+        [System.Web.Http.HttpPost]
+        public int StudentSave(Student student)
         {
-            return "Test data";
+            SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnection"].ConnectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("StudentSave", connection);
+            command.Parameters.AddWithValue("ID", student.ID);
+            command.Parameters.AddWithValue("Name", RandomString(5,false));
+            command.Parameters.AddWithValue("Password", student.Name);
+            command.Parameters.AddWithValue("Malayalam", student.Malayalam);
+            command.Parameters.AddWithValue("English", student.English);
+            command.Parameters.AddWithValue("Hindi", student.Hindi);
+            command.Parameters.AddWithValue("Email", student.Email);
+            command.CommandType = CommandType.StoredProcedure;
+            command.ExecuteNonQuery();
+            return 4;
+        }
+
+        public string RandomString(int size, bool lowerCase = false)
+        {
+            Random _random = new Random();
+            var builder = new StringBuilder(size);
+
+            char offset = lowerCase ? 'a' : 'A';
+            const int lettersOffset = 26; // A...Z or a..z: length=26  
+
+            for (var i = 0; i < size; i++)
+            {
+                var @char = (char)_random.Next(offset, offset + lettersOffset);
+                builder.Append(@char);
+            }
+
+            return lowerCase ? builder.ToString().ToLower() : builder.ToString();
         }
     }
 }
